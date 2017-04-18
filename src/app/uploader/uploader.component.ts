58e254118b9f74d84bdba12146ac2a51be196edc
Amin Azarbadegan <amin@azarbadegan.net>
2017-04-18 18:16:34 +0430

    add uploader component (adding the new scripts)

---
+++ src/app/uploader/uploader.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {FileUploader} from "ng2-file-upload";
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+@Component({
+  selector: 'app-uploader',
+  templateUrl: './uploader.component.html',
+  styleUrls: ['./uploader.component.css']
+})
+export class UploaderComponent{
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
+  }
+
+}
