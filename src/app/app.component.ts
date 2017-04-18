385be4b5b85c06d84035cfb5c071fa14af3a38d6
305-2 <saresalehi@yaoo.com>
2017-04-18 13:19:14 +0430

    add patient-index component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MessageService} from "./message.service";
-import {MdSnackBar} from "@angular/material";
+import {LoggedInGuard} from "./login/logged-in.guard";  //add some code
+import {MdSnackBar, MdSnackBarConfig} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit {
   showError = false;
   error: string;
 
-  constructor(private messageService: MessageService, public snackBar: MdSnackBar) {
+  constructor(private loggedInGuard: LoggedInGuard,private messageService: MessageService, public snackBar: MdSnackBar) {       //add some code
   }
 
   ngOnInit(): void {
@@ -25,12 +26,12 @@ export class AppComponent implements OnInit {
     this.messageService.msg$.subscribe(
       msg => {
         this.showError = false;
-        this.snackBar.open(msg, 'x', {duration: 3000, extraClasses: ['snackBar']});
+        this.snackBar.open(msg, 'x', <MdSnackBarConfig>{duration: 3000, extraClasses: ['snackBar']}); //change some code
       }
     );
     this.messageService.warn$.subscribe(
       msg => {
-        this.snackBar.open(msg, 'x', {duration: 3000, extraClasses: ['warnBar']});
+        this.snackBar.open(msg, 'x', <MdSnackBarConfig>{duration: 3000, extraClasses: ['warnBar']});  //change some code
       }
     )
   }
