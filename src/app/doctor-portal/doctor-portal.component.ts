90a49ea82bcdf3cdd9d5aeece7678e5a699b5583
Amin Azarbadegan <amin@azarbadegan.net>
2017-04-19 17:48:57 +0430

    Adding Doctors portal

--- src/app/doctor-portal/doctor-portal.component.ts
+++ src/app/doctor-portal/doctor-portal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RestService} from "../rest.service";
+import {PatientService} from "../patient.service";
 
 @Component({
   selector: 'app-doctor-portal',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctor-portal.component.css']
 })
 export class DoctorPortalComponent implements OnInit {
+  private firstname = "";
+  private surname: "";
+  private documents: "";
+  private notFound: boolean = true;
+  private paperId: any;
+  private startTime: any;
+  private vid: any;
+  private pid: number;
 
-  constructor() { }
+  constructor(private restService:RestService,private patientService:PatientService) { }
 
   ngOnInit() {
+    this.refresh();
+  }
+
+  private refresh() {
+    this.restService.get('my-visit').subscribe(
+      data => {
+        console.log(data);
+        this.firstname = data.patient.firstname;
+        this.surname = data.patient.surname;
+        this.startTime = data.start_time;
+        this.paperId = data.paper_id;
+        this.vid = data.vid;
+        this.pid = data.pid;
+        this.documents = data.documents;
+      },
+      err => {console.log(err);this.notFound = true;}
+    )
   }
 
 }
