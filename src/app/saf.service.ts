98ee8d9a937c8b235bea86f23c2d104e5fdf9feb
305-2 <saresalehi@yaoo.com>
2017-05-21 16:13:33 +0430

    add some code to safService to keep waiting patients list even after closing browser

--- src/app/saf.service.ts
+++ src/app/saf.service.ts
@@ -8,25 +8,30 @@ import {SocketService} from "./socket.service";
 import {Http, Response, URLSearchParams} from "@angular/http";
 import {WebSocketService} from 'angular2-websocket-service'
 import {isUndefined} from "util";
+import moment = require("moment");
 
 @Injectable()
 export class SafService{
   public safWatingForVisit = {};
-  public testObject = [];
 
   constructor(private restService: RestService,private http: Http, private socket: WebSocketService, private messageService:MessageService) {
     this.restService.get('waitingSaf').subscribe( data => {
-          this.testObject.push(data);
+          this.safWatingForVisit = {};
+          data.forEach( d => {
+            if(isUndefined(this.safWatingForVisit[d.did])){
+              this.safWatingForVisit[d.did] = [];
+            };
+            this.safWatingForVisit[d.did].push(d);
+          })
         },
         err => console.log(err)
     );
-
   }
 
 
   addPatientToSaf(data:any, callback) {
     this.restService.insert('waitingSaf/',data).subscribe(()=>{
-      if(isUndefined(this.safWatingForVisit[data.did])) {
+      if(isUndefined(this.safWatingForVisit[data.did])){
         this.safWatingForVisit[data.did] = [];
       };
       this.safWatingForVisit[data.did].push(data);
--- src/app/visit/visit.component.ts
+++ src/app/visit/visit.component.ts
@@ -14,14 +14,14 @@ import {SafService} from "../saf.service";
 })
 export class VisitComponent implements OnInit,OnDestroy {
   visits = [];
-  watings = [];
+  waitings = [];
   pid: number;
   x;
   enabled: boolean;
   currentVisit = [];
-  currentWating = [];
-  isVisitingOrWating: boolean;
-  isWating: boolean;
+  currentWaiting = [];
+  isVisitingOrWaiting: boolean;
+  isWaiting: boolean;
   isVisiting: boolean;
   canGo: boolean;
   doctors = [];
@@ -52,16 +52,17 @@ export class VisitComponent implements OnInit,OnDestroy {
     });
   }
 
+
   private getPatientId() {
     this.pidSub = this.patientService.pid$.subscribe(pid => {
       this.pid = pid;
       this.enabled = true;
       this.currentVisit = this.visits.filter(r => r.pid === this.pid);
-      this.currentWating = this.watings.filter(r=> r.pid === this.pid);
+      this.currentWaiting = this.waitings.filter(r=> r.pid === this.pid);
       this.isVisiting = this.currentVisit.length>0;
-      this.isWating = this.currentWating.length>0;
-      this.isVisitingOrWating = this.isVisiting || this.isWating;
-      this.canGo = !this.isVisitingOrWating ||(this.currentVisit.length>0 && this.authService.display_name === this.currentVisit[0].display_name);
+      this.isWaiting = this.currentWaiting.length>0;
+      this.isVisitingOrWaiting = this.isVisiting || this.isWaiting;
+      this.canGo = !this.isVisitingOrWaiting ||(this.currentVisit.length>0 && this.authService.display_name === this.currentVisit[0].display_name);
       this.isCurrentDoctorVisit = this.canGo && this.currentVisit.length>0;
       if (this.isCurrentDoctorVisit) {
         this.pageNumber = this.currentVisit[0].paper_id % 101 + 1;
@@ -97,11 +98,11 @@ export class VisitComponent implements OnInit,OnDestroy {
   }
 
   refresh() {
-    this.watings = [];
+    this.waitings = [];
     let a = this.safService.safWatingForVisit;
     for (let key in a) {
       for (var x = 0; x < a[key].length; x++) {
-        this.watings.push(a[key][x]);
+        this.waitings.push(a[key][x]);
       }
     }
     this.restService.get('active-visits').subscribe(
@@ -157,13 +158,13 @@ export class VisitComponent implements OnInit,OnDestroy {
         pid : this.pid,
         page_num : this.pageNumber,
         note_num : this.notebookNumber,
-        firstName : this.patientService.firstname,
-        surName : this.patientService.surname,
+        firstname : this.patientService.firstname,
+        surname : this.patientService.surname,
         waite_start_time : moment().format('HH:mm'),
         display_name: this.allDoctors.filter(r=>r.uid === this.doctor)[0].display_name,
       };
       this.safService.addPatientToSaf(data,()=>{
-        this.watings.push(data);
+        this.waitings.push(data);
         if(this.currentVisit.length && this.authService.display_name === this.currentVisit[0].display_name) { //referral by doctor
           this.endVisit(this.currentVisit[0].did, this.currentVisit[0].pid);
         }
