0d30b8ae893649612ca6bf12872757b2efc3ecb0
arhariri <ali.71hariri@gmail.com>
2017-08-19 12:31:20 +0430

    Create functionality for saving looked up and added patients in patientService (using localStoring on browser)

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -16,9 +16,12 @@ export class PatientService{
   private visitCache=[];
   pageNumber: number;
   notebookNumber: number;
+  private _tpList = [];
 
     constructor() {}
   newPatient(data:any) {
+    this.modifyTPList(data);
+
     this.pid = data.pid;
     this.firstname = data.firstname;
     this.surname = data.surname;
@@ -45,6 +48,66 @@ export class PatientService{
     return {
       display: this.visitCache.map(r=>{return {text:`${moment(r.started_at).format('HH:mm')} ${r.patient.firstname} ${r.patient.surname}`, sharingInfo: r.sharingInfo}}),
       values: this.visitCache.map(r=>r.vid),
+    }
+  }
+
+  initTPList(){
+    let tpList = JSON.parse(localStorage.getItem('tp_list'));
+    let currentDate = moment().format('YYYY-MM-DD');
+
+    if(tpList !== null && tpList !== undefined){
+      if(!(currentDate in tpList)){
+        let object = {};
+        object[currentDate] = [];
+        localStorage.setItem('tp_list', JSON.stringify(object));
+        this._tpList = [];
+      }
+
+      //Clear the previous dates if exist
+      for(let key of Object.keys(tpList)){
+        if(key !== currentDate)
+          delete tpList[key];
+      }
+    }
+    else
+      localStorage.setItem('tp_list', JSON.stringify({}));
+  }
+
+  getTPList(){
+    return this._tpList;
+  }
+
+  private modifyTPList(patientData: any){
+    let currentDate = moment().format('YYYY-MM-DD');
+    let tpObject = JSON.parse(localStorage.getItem('tp_list'));
+    if(tpObject){
+      if(tpObject[currentDate] && tpObject[currentDate].find(el => el.pid === patientData.pid))
+        this.updateTPList(patientData, tpObject, currentDate);
+      else
+        this.insertToTPList(patientData, tpObject, currentDate);
+    }
+  }
+
+  private insertToTPList(patientData: any, tpObject, date){
+    if(tpObject){
+      this._tpList = tpObject[date];
+      this._tpList.push(patientData);
+      localStorage.setItem('tp_list', JSON.stringify(tpObject));
+    }
+  }
+
+  private updateTPList(patientData: any, tpObject, date){
+    if(tpObject){
+      this._tpList = tpObject[date];
+      let targetPatient = this._tpList.find(el => el.pid === patientData.pid);
+      targetPatient.firstname = patientData.firstname;
+      targetPatient.surname = patientData.surname;
+      targetPatient.dob = patientData.dob;
+      targetPatient.id_number = patientData.id_number;
+      targetPatient.contact_details = patientData.contact_details;
+      targetPatient.pageNumber = null;
+      targetPatient.notebookNumber = null;
+      localStorage.setItem('tp_list', JSON.stringify(tpObject));
     }
   }
 }
