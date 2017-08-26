fa26e5948adf3bec4b920285388472c36b2d0076
Amin Azarbadegan <amin@azarbadegan.net>
2017-08-26 11:21:49 +0430

    Fixing bug #15

--- src/app/visit.service.ts
+++ src/app/visit.service.ts
@@ -254,6 +254,10 @@ export class VisitService {
                     () => this.msg.message('New Waiting'),
                     err => console.warn('Error in creating new visit: ' + err)
                   );
+                }
+                else if (+originLoc === 1) {
+                  this.msg.warn('You cannot remove a visit after it started');
+                  this.resetPCard();
                 } else { // Not permitted
                   this.msg.warn('You should first put the patient in the queue');
                   this.resetPCard();
